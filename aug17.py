5dca46e94fa8b4dfd898e1d82577fc944e6eba70
Prince raj sah <130084631+princeshah01@users.noreply.github.com>
2024-08-25 18:18:45 +0530

    Add files via upload

---
+++ aug17.py
@@ -0,0 +1,34 @@
+name = "prince"
+age = 23.50
+print(f"my name is {name} and my age is {age}")   
+# we can use this f formating to format varible name in our string
+
+print("my name is %s and my age is %d" %(name , age))
+
+# we can also use % sign to format our string %s used for str %d is used for int %f is used for float %x is used for hexadecimal
+
+print("my name is {} and my age is {} " .format(name ,age))
+
+# we can also use str.format to format out string or output
+
+
+# for  string we use "" double , single and multi quotation 
+
+name1 = "prince"
+name2 = 'prince'
+name3 = '''prince raj sah '''
+
+# we use snake_case , camelCase ,  PascalCase , kabeb-case to create variable , function and class name 
+
+# we use hash symbol to create single line comments on our code 
+
+""" 
+we use this 3 time double quotation 
+to create 
+multiple line comments by the way  this 3 time single quot 
+and 3 time double quot is treated as sting but python ignores 
+those string which is not assigned to any varible 
+
+"""
+
+
---
+++ aug18.py
@@ -0,0 +1,34 @@
+# in this filw we will learn about datatype and variable 
+
+name = "prince"
+
+# above name is a varible which mainly used to store data of type str .
+# name is now refering to a memory location where "prince " is stored . 
+
+nameOne ="prince"
+
+# now both name and nameOne is  refering to a same memory location . 
+# there are 
+"""
+1. int 
+2. float 
+3. string 
+4. boolen
+5. list 
+6. dict
+7. tuple
+
+"""
+number = 20
+print(type(number))
+
+# in python if we will assign multiple value to a single vaarible then it will become tuple eg :-
+
+a = 1,2,3,"prince","raj"
+print(type(a))
+
+a=b=c = 1
+# 
+print(a)
+
+
